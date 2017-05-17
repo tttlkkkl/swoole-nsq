76d3a72add3a9f07d097bc7466c3a317334d979f
lihs <yehong0000@163.com>
2017-05-17 11:34:44 +0800

    客户端库更新

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 #swoole-nsq
+基于swoole的NSQ消费实例。本实例依赖nsq客户端类库：[oschina](https://git.oschina.net/tttlkkkl/nsq_swoole_client),[github](https://github.com/tttlkkkl/nsq_swoole_client)。
+
 ```
 cd path/test
 php NsqClient.php -s 1
--- lib/opt/nsq/lib/client/Client.php
+++ lib/opt/nsq/lib/client/Client.php
@@ -217,6 +217,7 @@ class Client implements ClientInterface {
         if (Unpack::isHeartbeat($frame)) {
             $client->send(Packet::nop());
             $this->Log->debug('心跳查询');
+            return 1;
         } elseif (Unpack::isOk($frame)) {
             $this->init['ok'] += 1;
             $this->Log->debug('成功响应:' . $frame['msg']);
@@ -224,13 +225,16 @@ class Client implements ClientInterface {
             if ($response === $this->init['response'] && 1 === $this->init['ok']) {
                 $this->Log->info('订阅成功，开始第一条消费');
                 $client->send(Packet::rdy(1));
+                return 1;
             }
         } elseif (Unpack::isError($frame)) {
             $this->Log->warn('错误响应' . $frame['msg']);
+            return 1;
         } elseif (Unpack::isMessage($frame)) {
-            $this->Log->debug('收到消费消息:' . $frame['msg']);
+            $this->Log->error('收到消费消息:' . $frame['msg']);
             $this->handleMessage($client, $frame);
             $client->send(Packet::rdy(1));
+            return 1;
         } elseif (Unpack::isResponse($frame)) {
             $identify = json_decode($frame['msg'], true);
             if (isset($identify['auth_required'])) {
@@ -243,8 +247,10 @@ class Client implements ClientInterface {
             }
             //收到正确的授权结果
             if (isset($identify['permission_count']) && $identify['permission_count'] > 0) {
+                $this->Log->info('收到授权结果信息:' . $frame['msg']);
                 $this->isAuth = true;
             }
+            return 1;
         } else {
             $this->Log->warn('未知的响应');
         }
@@ -290,7 +296,8 @@ class Client implements ClientInterface {
      *
      * @param Client $client
      *
-     * @return mixed
+     * @return mixedsudo /usr/local/nsq/bin/nsqd -config=/usr/local/nsq/bin/nsqd.cfg --auth-http-address=127.0.0.1:9005
+
      */
     public function onClose(SwooleClient $client) {
         $this->Log->warn('连接断开...');
@@ -342,7 +349,6 @@ class Client implements ClientInterface {
         if ($this->Dedupe->add($this->topic, $this->channel, $message)) {
             $this->Log->debug('重复消息：' . json_encode($frame));
             $client->send(Packet::fin($message->getId()));
-            $client->send(Packet::rdy(1));
         } else {
             try {
                 $handle = $this->Handle->handle($message);
@@ -352,7 +358,6 @@ class Client implements ClientInterface {
             if ($handle) {
                 $this->Log->info('消息处理完毕:' . json_encode($frame));
                 $client->send(Packet::fin($message->getId()));
-                $client->send(Packet::rdy(1));
                 $this->Log->debug('准备接收消息');
             } else {
                 $this->Log->error('消息处理出错:' . json_encode($frame));
@@ -364,6 +369,7 @@ class Client implements ClientInterface {
                         $this->Log->debug('消息排队失败:' . json_encode($frame));
                     }
                 } else {
+                    $client->send(Packet::fin($message->getId()));
                     $this->Log->info('消息被丢弃:' . json_encode($frame));
                 }
             }
